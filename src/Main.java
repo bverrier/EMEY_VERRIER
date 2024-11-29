fd71cf9ef1646d3306a6397e14252a3c4912533f
bverrier <bastien.verrier@outlook.fr>
2024-11-29 16:17:31 +0100

    Fin

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Création d'un purse standard, PIN user = 1234, PIN admin = 123456
+        Purse purse = new Purse(new int[] {1, 2, 3, 4}, new int[] {1, 2, 3, 4, 5, 6});
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Opération de crédit : 30 euros");
+        purse.beginTransactionCredit(30);
+        purse.commitTransactionCredit();
+
+        System.out.println("Affichage du solde : "+purse.getData());
+
+        System.out.println("Opération de débit : 20 euros");
+        purse.beginTransactionDebit(20);
+        purse.commitTransactionDebit();
+
+        System.out.println("Affichage du solde : "+purse.getData());
+
+        System.out.println("Déblocage de la carte");
+        purse.PINChangeUnblock();
+
+        System.out.println("Opération de crédit : 15 euros");
+        purse.beginTransactionCredit(15);
+        purse.commitTransactionCredit();
+
+        System.out.println("Opération de débit : 10 euros");
+        purse.beginTransactionDebit(10);
+        purse.commitTransactionDebit();
     }
 }
--- src/Purse.java
+++ src/Purse.java
@@ -1,2 +1,208 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Purse {
+    enum LCS{
+        PRE_PERSO, USE, BLOCKED, DEAD
+    }
+
+    private int[] userPIN;
+    private int[] adminPIN;
+    private final int MAX_USER_TRIES;
+    private final int MAX_ADMIN_TRIES;
+    private final int MAX_TRANS;
+    private final int MAX_BALANCE;
+    private final int MAX_CREDIT_AMOUNT;
+    private final int MAX_DEBIT_AMOUNT;
+
+    private int userTriesLeft;
+    private int adminTriesLeft;
+    private int balance;
+    private int copy;
+    private int transLeft;
+    private boolean userAuthenticate;
+    private boolean inTransaction;
+    private boolean adminAuthenticate;
+    private LCS lifeCycleState;
+
+    Purse(int MAX_USER_TRIES, int MAX_ADMIN_TRIES, int MAX_TRANS, int MAX_BALANCE, int MAX_CREDIT_AMOUNT, int MAX_DEBIT_AMOUNT, int[] userPIN, int[] adminPIN){
+        this.MAX_USER_TRIES = MAX_USER_TRIES;
+        this.MAX_ADMIN_TRIES = MAX_ADMIN_TRIES;
+        this.MAX_TRANS = MAX_TRANS;
+        this.MAX_BALANCE = MAX_BALANCE;
+        this.MAX_CREDIT_AMOUNT = MAX_CREDIT_AMOUNT;
+        this.MAX_DEBIT_AMOUNT = MAX_DEBIT_AMOUNT;
+        this.userPIN = userPIN;
+        this.adminPIN = adminPIN;
+        initialize();
+        reset();
+    }
+
+    public Purse(int[] userPIN, int[] adminPIN) {
+        this(3, 4, 500, 100, 50, 30, userPIN, adminPIN);
+    }
+
+    private void initialize() {
+        this.userTriesLeft = MAX_USER_TRIES;
+        this.adminTriesLeft = MAX_ADMIN_TRIES;
+        this.balance = 0;
+        this.transLeft = MAX_TRANS;
+        this.userAuthenticate = false;
+        this.adminAuthenticate = false;
+        this.lifeCycleState = LCS.USE;
+    }
+
+    boolean verifyPINUser(int[] PINCode){
+        if (lifeCycleState != LCS.USE || userTriesLeft <= 0) {
+            return false;
+        }
+        if (Arrays.equals(userPIN, PINCode)) {
+            userTriesLeft = MAX_USER_TRIES;
+            return true;
+        } else {
+            userTriesLeft--;
+            if (userTriesLeft == 0) {
+                lifeCycleState = LCS.BLOCKED;
+            }
+            return false;
+        }
+    }
+
+    boolean verifyPINAdmin(int[] PINCode){
+        if (lifeCycleState == LCS.DEAD || adminTriesLeft <= 0) {
+            return false;
+        }
+        if (Arrays.equals(PINCode, adminPIN)) {
+            adminTriesLeft = MAX_ADMIN_TRIES;
+            adminAuthenticate = true;
+            return true;
+        } else {
+            adminTriesLeft--;
+            if (adminTriesLeft == 0) {
+                lifeCycleState = LCS.DEAD;
+            }
+            return false;
+        }
+    }
+
+    private boolean getIdentificationAdmin(){
+        System.out.println("Veuillez entrer le code PIN administrateur :");
+        Scanner scanner = new Scanner(System.in);
+        for(int loop = 0 ; loop < MAX_ADMIN_TRIES; loop++){
+            String input = scanner.nextLine();
+            int[] inputPIN = new int[input.length()];
+            for (int i = 0; i < input.length(); i++) {
+                inputPIN[i] = Character.getNumericValue(input.charAt(i));
+            }
+            if(verifyPINAdmin(inputPIN)){
+                adminAuthenticate = true;
+                break;
+            }else{
+                System.out.println("Code PIN incorrect, veuillez réessayer.");
+            }
+        }
+        return adminAuthenticate;
+    }
+
+    private boolean getIdentificationUser(){
+        if(lifeCycleState != LCS.USE){
+            System.out.println("La carte n'est pas utilisable actuellement.");
+            return false;
+        }
+        System.out.println("Veuillez entrer le code PIN :");
+        Scanner scanner = new Scanner(System.in);
+        for(int loop = 0 ; loop < MAX_USER_TRIES; loop++){
+            String input = scanner.nextLine();
+            int[] inputPIN = new int[input.length()];
+            for (int i = 0; i < input.length(); i++) {
+                inputPIN[i] = Character.getNumericValue(input.charAt(i));
+            }
+            if(verifyPINUser(inputPIN)){
+                userAuthenticate = true;
+                break;
+            }else{
+                System.out.println("Code PIN incorrect, veuillez réessayer.");
+            }
+        }
+        return userAuthenticate;
+    }
+
+    void PINChangeUnblock(){
+        if (getIdentificationAdmin() && lifeCycleState == LCS.BLOCKED) {
+            userTriesLeft = MAX_USER_TRIES;
+            lifeCycleState = LCS.USE;
+        }
+    }
+
+    void beginTransactionDebit(int amount){
+        try {
+            if (lifeCycleState != LCS.USE || transLeft <= 0 || amount > MAX_DEBIT_AMOUNT || amount > balance) {
+                throw new IllegalStateException("Transaction non autorisée.");
+            }
+            inTransaction = true;
+            copy = balance;
+            balance -= amount;
+        } catch (Exception e) {
+            System.out.println("Erreur pendant la transaction");
+            reset();
+        }
+
+    }
+
+    void beginTransactionCredit(int amount){
+        try{
+            if (lifeCycleState != LCS.USE || transLeft <= 0 || amount > MAX_CREDIT_AMOUNT || balance + amount > MAX_BALANCE) {
+                throw new IllegalStateException("Transaction non autorisée.");
+            }
+            if (getIdentificationUser()) {
+                inTransaction = true;
+                copy = balance;
+                balance += amount;
+            } else {
+                throw new IllegalStateException("Authentification utilisateur requise.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erreur pendant la transaction");
+            reset();
+        }
+    }
+
+    void commitTransactionDebit(){
+        if (lifeCycleState == LCS.USE) {
+            transLeft--;
+            checkCardStatus();
+            inTransaction = false;
+        }
+    }
+    void commitTransactionCredit(){
+        if (lifeCycleState == LCS.USE) {
+            transLeft--;
+            userAuthenticate = false;
+            checkCardStatus();
+            inTransaction = false;
+        }
+    }
+
+    private void checkCardStatus() {
+        if (transLeft <= 0) {
+            lifeCycleState = LCS.DEAD;
+        }
+    }
+
+    int getData(){
+        return balance;
+    }
+
+    void reset() {
+        if (inTransaction) {
+            abortTransaction();
+        }
+    }
+
+    void abortTransaction() {
+        balance = copy;
+        inTransaction = false;
+        System.out.println("Transaction annulée");
+        reset();
+    }
 }
